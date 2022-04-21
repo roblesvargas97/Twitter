d62adc42365075cb1cf9e9b4569fae3d034d9e18
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-21 02:08:45 -0500

    Add test and method getInfo

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,11 @@ class UserService {
   static create(id, userName, name) {
     return new User(id, userName, name, "Sin Bio");
   }
+
+  static getInfo(user) {
+    const { id, userName, name, bio } = user;
+    return [id, userName, name, bio];
+  }
 }
 
 module.exports = UserService;
--- test/services/UserServices.test.js
+++ test/services/UserServices.test.js
@@ -2,10 +2,19 @@ const UserService = require("../../app/services/UserService");
 
 describe("Test for UserService", () => {
   const user = UserService.create(1, "jesus999", "Jesus");
-  it("should ", () => {
+  test("Create a new user Using the UserService ", () => {
     expect(user.userName).toBe("jesus999");
     expect(user.name).toBe("Jesus");
     expect(user.id).toBe(1);
     expect(user.bio).not.toBeUndefined();
+  });
+
+  test(" Get all user data in list", () => {
+    const user = UserService.create(1, "jesus999", "Jesus");
+    const userInfoList = UserService.getInfo(user);
+    expect(userInfoList[0]).toBe(1);
+    expect(userInfoList[1]).toBe("jesus999");
+    expect(userInfoList[2]).toBe("Jesus");
+    expect(userInfoList[3]).toBe("Sin Bio");
   });
 });
