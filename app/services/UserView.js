a558b7e0276a5a69c33c97543a3e749f56408d3a
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-22 22:29:06 -0500

    Create Class UserView

---
+++ app/services/UserView.js
@@ -0,0 +1,11 @@
+class UserView {
+  static createUser(payload) {
+    if (payload === null) {
+      return {
+        error: "payload no existe",
+      };
+    }
+  }
+}
+
+module.exports = UserView;
